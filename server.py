a314daf087b3b30a1d7c113cc0b2d4fd6bac0675
myth <maystya@gmail.com>
2015-08-27 07:44:54 +0700

    default setting

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import select
 import socket
 import sys
 import struct
+from SensorHandler import *
 
 #global declaration port, ip, sensor_name
 aports = 0
@@ -54,9 +55,50 @@ def file_read():
 
 #request handler
 def serve(ports):
-    listeners, socket = [], []
+    baca_sensor = SensorHandler()
+    listeners, sockets = [], []
     for port in ports:
-        listener 
+        listener = socket.socket()
+        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        listener.bind(('127.0.0.1', port))
+        listener.setblocking(False)
+        listener.listen(5)
+        listeners.append(listener)
+        sockets.append(listener)
+        print(listener.getsockname(), ' listening')
+    while True:
+        r, w, x = select.select(sockets, sockets, sockets)
+        for sock in r:
+            if sock in listeners:
+                c, a = sock.accept()
+                print(sock.getsockname(), ' <- ', a)
+                aports, aips, asensors=file_read()
+                dport=sock.getsockname()
+                if int (dport[1]) == 2222:
+                    pesan = baca_sensor.api()
+                    c.send(str(pesan))
+                elif int (dport[1]) == 2223:
+                    baca_sensor.suhu()
+                    c.send('Nih data suhunya')
+                sockets.append(c)
+            else:
+                buf = sock.recv(80)
+                if not buf:
+                    print(sock.getpeername(), ' EOF')
+                    sockets.remove(sock)
+                else:
+                    print(sock.getpeername(), ' <- ', buf)
+        for sock in w:
+            pass
+        for sock in x:
+            print(sock.getpeername(), ' error')
+            sockets.remove(sock)
 
 if __name__ == '__main__':
-    file_read()
+
+    translasi = file("translasi.txt", 'r')
+    cols, indexToName = getColumns(translasi)
+    translasi.close()
+    #serve(cols['PORT'],cols['SENSOR'])
+    serve (int(arg) for arg in cols['PORT'])
+    #serve(int(arg) for arg in sys.argv[1:])
